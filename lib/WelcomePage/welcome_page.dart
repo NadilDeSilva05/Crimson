141743c92220cf910d464ebe36208527320ba7ed
bawanisandunika <146722385+bawanisandunika@users.noreply.github.com>
2024-05-26 15:15:21 +0530

    changed by bawani
    welcome page edit

---
+++ lib/WelcomePage/welcome_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/crimson_logo.png', // Ensure the logo asset is in the correct path
+                height: 100,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'CRIMSON +\nBlood Bank',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 117, 44, 39),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to Crimson PVT.',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Connect with donors, find donation opportunities, and stay updated on local drives. Thank you for making a difference!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle button press
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                child: Text(
+                  'GET STARTED',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
