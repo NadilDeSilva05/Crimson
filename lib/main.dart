49a19e031c1012bdeb41388d5d5899efa15e16f6
Nadil De Silva <161411320+NadilDeSilva05@users.noreply.github.com>
2024-05-26 18:45:52 +0530

    donow

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:crimson/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
