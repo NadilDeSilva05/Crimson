d1f85d500eadb0e55f869976c32e9b1aa40d7991
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-05-26 13:50:33 +0530

    Edit EmergencyContacts

--- lib/EmergencyContacts/EmergencyContacts.dart
+++ lib/EmergencyContacts/EmergencyContacts.dart
@@ -1,7 +1,19 @@
-
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) =>
+          const BloodDonorScreen(), // Wrap your main app with DevicePreview
+    ),
+  );
+}
 
 class BloodDonorScreen extends StatelessWidget {
   const BloodDonorScreen({super.key});
