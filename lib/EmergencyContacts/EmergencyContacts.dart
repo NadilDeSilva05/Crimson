c82c3746f4b47a5247056e38cb078be8790894f7
Nadil De Silva <161411320+NadilDeSilva05@users.noreply.github.com>
2024-05-27 18:02:44 +0530

    Update EmergencyContacts.dart

--- lib/EmergencyContacts/EmergencyContacts.dart
+++ lib/EmergencyContacts/EmergencyContacts.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: file_names
-
 import 'package:crimson/Homepage/homepage.dart';
 import 'package:flutter/material.dart';
 //import 'main.dart'; // Import the main.dart file where HomePage is defined
